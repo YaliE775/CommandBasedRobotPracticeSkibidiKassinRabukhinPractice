aa02a0837c89d0d67ac6bccff592ab60404b1f0f
SycaTech <yalienchoa@gmail.com>
2024-08-04 19:04:59 +0300

    add(Tank.java, TankDriveCommand): add tank subsystem and drive command

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    public static class TankConstants {
+        public static int MASTER_RIGHT_ID = 4;
+        public static int MASTER_LEFT_ID = 2;
+
+        public static int SLAVE_RIGHT_ID = 3;
+        public static int SLAVE_LEFT_ID = 1;
+    }
+
+    public static class JoystickConstants {
+        public static int DRIVER_PORT = 0;
+
+    }
+}
